17055e6831bf1267432184475e770c70be5cbc4d
Erick Reyna <erred93@gmail.com>
2018-08-27 13:56:13 -0500

    Update main method to print the total amount of invoices found and the number of calls

--- lib/simple_fetcher.rb
+++ lib/simple_fetcher.rb
@@ -1,7 +1,16 @@
 require_relative 'data_fetcher'
 class SimpleFetcher
 
+    # Main method, expects a map with id, start and finish values.
+    # Prints the total amount of invoices found of a given range
     def self.run(args)
+
+        return nil if !ARGS[:start] or !ARGS[:finish] or !ARGS[:id]
+
+        fetcher = DataFetcher.new(id: args[:id], host: args[:host])
+        total_invoices = fetcher.get_invoice_count(args[:start],args[:finish],0)
+        p "Amount of invoices found: #{total_invoices}"
+        p "Number of calls: #{fetcher.calls}"
     end
 
 end
